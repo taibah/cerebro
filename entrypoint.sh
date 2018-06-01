993000e9ac566cf264a693f09666aeedafbb931c
Justin Henderson <jhenderson@tekrefresh.com>
2018-06-01 11:45:48 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sed -i "s/basePath = \"\/\"/basePath = \"$(echo $BASEPATH | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')\"/g" /opt/cerebro/conf/application.conf
-/opt/cerebro/bin/cerebro
 sed -i "s/http:\/\/elasticsearch:9200/$PROTOCOL:\/\/$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/g" /opt/cerebro/conf/application.conf
-mkdir /opt/cerebro/logs
+
 /opt/cerebro/bin/cerebro
